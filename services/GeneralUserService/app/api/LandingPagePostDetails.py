c786db7244afc7097f3b63fc0368b573f6deb193
dumidulkdev <dumi.dev.lk@gmail.com>
2026-01-23 20:05:51 +0530

    add: landing page single post limited details return setup added

--- services/GeneralUserService/app/api/LandingPagePostDetails.py
+++ services/GeneralUserService/app/api/LandingPagePostDetails.py
@@ -1,4 +1,10 @@
-from fastapi import APIRouter
+from fastapi import APIRouter,Depends
+from typing import Annotated
+from supabase import Client
+from app.database.supabase import get_supabase_client
+from services.GeneralUserService.app.services.LandingPagePostDetailsService import (
+    GetLandingPagePostDetailsService)
+
 
 """
 NOTE controller task : in this controller i fetch limited details from post that loaded in to the landing page.
@@ -8,6 +14,6 @@ not authorized user cannot see the whole details. this endpoint only returns lim
 router  = APIRouter(tags=["General user routes"])
 
 @router.get("/landing-feed-post-details/{property_id}",description="this return landing page properties details with limited setup")
-def PostDetailsController():
-    pass
+def PostDetailsController(property_id:str,db: Annotated[Client, Depends(get_supabase_client)]):
+    return GetLandingPagePostDetailsService(db=db,post_id=property_id)
 
